b0d40a1ca64d3b0775fe6bb4e286387de952d04d
Angela <angela@littledipper.com>
2019-02-02 19:07:56 -0500

    Audio working!

--- android/app/src/main/java/com/example/dysonsphere/MainActivity.java
+++ android/app/src/main/java/com/example/dysonsphere/MainActivity.java
@@ -2,13 +2,126 @@ package com.example.dysonsphere;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int MEDIA_RES_ID = R.raw.sample_audio;
+
+    private TextView mTextDebug;
+    private SeekBar mSeekbarAudio;
+    private PlayerAdapter mPlayerAdapter;
+    private boolean mUserIsSeeking = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.fragment_layout);
+
+        initializeUI();
+        initializeSeekbar();
+        initializePlaybackController();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mPlayerAdapter.loadMedia(MEDIA_RES_ID);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mPlayerAdapter.release();
+    }
+
+    private void initializeUI() {
+        Button mPlayButton = (Button) findViewById(R.id.button_play);
+        Button mPauseButton = (Button) findViewById(R.id.button_pause);
+        Button mResetButton = (Button) findViewById(R.id.button_reset);
+        mSeekbarAudio = (SeekBar) findViewById(R.id.seekbar_audio);
+
+        mPauseButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mPlayerAdapter.pause();
+                    }
+                });
+        mPlayButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mPlayerAdapter.play();
+                    }
+                });
+        mResetButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mPlayerAdapter.reset();
+                    }
+                });
+    }
+
+    private void initializePlaybackController() {
+        MediaPlayerHolder mMediaPlayerHolder = new MediaPlayerHolder(this);
+        mMediaPlayerHolder.setPlaybackInfoListener(new PlaybackListener());
+        mPlayerAdapter = mMediaPlayerHolder;
+    }
+
+    private void initializeSeekbar() {
+        mSeekbarAudio.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    int userSelectedPosition = 0;
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                        mUserIsSeeking = true;
+                    }
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        if (fromUser) {
+                            userSelectedPosition = progress;
+                        }
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                        mUserIsSeeking = false;
+                        mPlayerAdapter.seekTo(userSelectedPosition);
+                    }
+                });
+    }
+
+    public class PlaybackListener extends PlaybackInfoListener {
+
+        @Override
+        public void onDurationChanged(int duration) {
+            mSeekbarAudio.setMax(duration);
+        }
+
+        @Override
+        public void onPositionChanged(int position) {
+            if (!mUserIsSeeking) {
+                mSeekbarAudio.setProgress(position, true);
+            }
+        }
+
+        @Override
+        public void onStateChanged(@State int state) {
+            String stateToString = PlaybackInfoListener.convertStateToString(state);
+            onLogUpdated(String.format("onStateChanged(%s)", stateToString));
+        }
+
+        @Override
+        public void onPlaybackCompleted() {
+        }
     }
 }
