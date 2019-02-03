3ad2a2a89d9b52a9a87f188b8a7e8d4a665993eb
Zi Kai Chen <zikaichen@outlook.com>
2019-02-03 11:48:14 -0500

    french

--- android/app/src/main/java/com/example/dysonsphere/MainActivity.java
+++ android/app/src/main/java/com/example/dysonsphere/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.dysonsphere;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ScrollView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        setSupportActionBar((Toolbar) findViewById(R.id.appbar_toolbar));
 
         initializeUI();
         initializeSeekbar();
@@ -38,6 +41,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         mPlayerAdapter.release();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_language) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setClassName("com.android.settings", "com.android.settings.LanguageSettings");
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void initializeUI() {
